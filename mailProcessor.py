1dc632748dafc54aa0caf26b6c99e5379c2b5a89
Stephen LaPlante <laplansk@gmail.com>
2015-05-26 02:59:50 -0700

    finished parser for raw email data in mbox file

--- mailProcessor.py
+++ mailProcessor.py
@@ -3,30 +3,43 @@ import mailbox
 import mail_utils
 import os.path
 
-
-#mboxfile = '../mail_data/smallFile0.mbox'
+df_name = "mail_data/training_data.mail"
 mboxfile = sys.argv[1]
-
+unlikely_string = "#!#!#!#!\n"
 # check that command line arg is valid filepath
 if not os.path.isfile(mboxfile):
     print('Invalid filepath - exiting')
     sys.exit(1)
 
-print(mboxfile)
-messageNum = 1
-payload = None
-
-for msg in mailbox.mbox(mboxfile):
-    if mail_utils.hasAttachment(msg) is not None:
-        print "message " + str(messageNum) + " with attachment\n"
-        messageNum += 1
-        print "Subject: " + mail_utils.displaySubject(msg) + "\n"
-        if mail_utils.getPlainTextBody(msg) is None:
-            print "Body:\n EMPTY MESSAGE BODY\n"
-        else:
-            print "Body:\n" + mail_utils.getPlainTextBody(msg) + "\n"
-
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print('Usage: python parser.py [mbox file]')
         sys.exit(1)
+
+print(mboxfile)
+dest = open(df_name, "a")
+
+
+i = 1
+for msg in mailbox.mbox(mboxfile):
+    print "processing message " + str(i)
+    dest.write(unlikely_string)
+    if not mail_utils.hasBody(msg):
+        continue
+
+    # YES or NO for having attachment
+    if mail_utils.hasAttachment(msg):
+        dest.write("YES\n")
+    else:
+        dest.write("NO\n")
+
+    # write out the body of the email
+    dest.write(mail_utils.getPlainText(msg) + "\n")
+    i += 1
+
+dest.close()
+
+
+
+
+
--- mail_utils.py
+++ mail_utils.py
@@ -1,29 +1,49 @@
-import mailbox
 import re
 
-def displayMessageComponents(msg):
-    for part in msg.walk():
-        print part.get_content_type()
-
-# This function returns true if the msg parameter has an attachment of the type application/*
 def hasAttachment(msg):
-    for part in msg.walk():
-        pattern = re.compile('application/*')
-        if re.search(pattern, part.get_content_type()) is not None:
-            return part.get_content_type()
+    pattern = re.compile('application/*')
+    if msg.is_multipart():
+        for part in msg.walk():
+            if re.search(pattern, part.get_content_type()) is not None:
+                return True
+    elif re.search(pattern, msg.get_content_type()) is not None:
+        return True
+    else:
+        return False
 
-# this function returns the plain text message of an email if such text exists.
-# If the email was empty, it returns the empty string
-def getPlainTextBody(msg):
-    for part in msg.walk():
-        pattern = re.compile('text/plain')
-        if re.search(pattern, part.get_content_type()) is not None:
-            return part.get_payload()
+
+# This function returns true if the msg parameter has body text
+def hasBody(msg):
+    if msg.is_multipart():
+        for part in msg.walk():
+            if part.get_content_type() == "text/plain":
+                return True
+    elif msg.get_content_type() == "text/plain":
+        return True
+    else:
+        return False
 
 # this function prints all first-level components of the msg parameter separated by newlines
 def displayMessageComponents(msg):
-    for part in msg.walk():
-        print part.get_content_type()
+    if msg.is_multipart():
+        print "Multi:"
+        for part in msg.walk():
+            print "\t" + part.get_content_type()
+            # print part.get_payload()
+            # print ""
+    else:
+        print "Simple:"
+        print msg.get_content_type()
+
+# returns any plain text from msg. If email was void f p
+def getPlainText(msg):
+    if msg.is_multipart():
+        for part in msg.walk():
+            if part.get_content_type() == "text/plain":
+                return part.get_payload()
+    else:
+        if msg.get_content_type() == "text/plain":
+            return msg.get_payload()
 
 # this function returns the subject of the msg parameter
 def displaySubject(msg):
